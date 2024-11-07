584f80a74dbff3e747331f93e683b739d3a45157
Jenarsav <u2131666@utp.edu.pe>
2024-11-07 17:12:27 -0500

    Correcion

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://127.0.0.1:5500'],  // Aquí agregas tu frontend
+    'allowed_origins' => ['http://localhost:4200'],  // Aquí agregas tu frontend
 
     'allowed_origins_patterns' => [],
     
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\NoticeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('notices', NoticeController::class);
+Route::resource('/notices', NoticeController::class);
